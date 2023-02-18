6b275d6cbf9b843dbae58cb69d1ad0d19cd44de6
Xavier Monzonís Hernández <pome@MacPome.local>
2023-02-18 22:05:27 +0100

    fixed seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -4,33 +4,37 @@
 import sys, re
 from argparse import ArgumentParser
 
-# arguments
+# input
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = 
 "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
+# classify the sequence
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+args.seq = args.seq.upper()  
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+    if re.search('T', args.seq) and not re.search('U', args.seq):
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq) and not re.search('T', args.seq):
         print ('The sequence is RNA')
+    elif re.search('U', args.seq) and re.search('T', args.seq):
+        print ('The sequence is not DNA nor RNA')
     else:
         print ('The sequence can be DNA or RNA')
-else:
-    print ('The sequence is not DNA nor RNA')
+# adding conditions with "and not" and "and" we can improve the sequence classifier
 
+
+# find motif in the sequence
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("found")
+        print("FOUND!")
     else:
-        print("not found")
+        print("NOT FOUND!")
